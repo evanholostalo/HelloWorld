d25cd64a42278ff5170e720d6bc4407927d320dd
Madasin Garrison <mgarrison@pssolutions.net>
2024-10-23 19:15:13 -0400

    decoder method

--- Lab6/encoder.py
+++ Lab6/encoder.py
@@ -1,12 +1,20 @@
 #Evan Ho Lostalo
+
 def encode(password):
     encoded = ''
     for char in password:
         encoded += str((int(char) + 3) % 10)  # Shift the digit up by 3 and handle wrap-around
     return encoded
 
+def decode(pass_hold):
+    print(pass_hold)
+    decoded = ''
+    for char in pass_hold:
+        decoded += str((int(char) - 3) % 10)
+    print("\nThe encoded password is", pass_hold, "and the original password is", decoded + '.')
 
 def main():
+    hold_pass = ""
     while True:
         print("Menu\n-------------")
         print("1. Encode")
@@ -16,17 +24,15 @@ def main():
 
         if choice == "1":
             password = input("Please enter your password to encode: ")
-            encoded_password = encode(password)
+            hold_pass = encode(password)
             print("Your password has been encoded and stored!")
 
         elif choice == "2":
-            print("Decoder function not implemented yet.")
-
+            decode(hold_pass)
         elif choice == "3":
             break
         else:
             print("Invalid choice. Please choose again.")
-
 
 if __name__ == "__main__":
     main()
